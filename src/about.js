896003d20ccc5510f0a03cf44b885c3f254245d7
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-04-10 22:44:55 +0700

    Add about content

--- src/about.js
+++ src/about.js
@@ -6,8 +6,15 @@ const about = {
         navAbout.classList.add('tab-active');
         const content = document.querySelector('#content');
         content.innerHTML = '';
+
+        const title = document.createElement('h1');
+        title.textContent = 'About Page';
+        title.classList.add('text-center', 'text-3xl', 'mb-4');
+
         const introduction = document.createElement('p');
-        introduction.textContent = 'About Page';
+        introduction.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quia consequuntur doloremque cupiditate, provident similique illo aperiam eligendi ut quod!';
+
+        content.appendChild(title);
         content.appendChild(introduction);
     }
 }
